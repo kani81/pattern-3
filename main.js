a9663ee884df2aac2cdc39fa9406d11cfd9315b3
kani81 <kaniarasiduraisamy.com>
2023-07-18 14:43:22 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,15 @@
+var n=prompt("enter the value");
+
+for (let i=1; i<=n; i++){
+		for(let j=i; j<=n; j++){
+			
+			document.write("&nbsp ");
+		}
+		for(let k=1;k<=i;k++){
+			document.write("*");
+		}
+		for(let l=1; l<=i; l++){
+			document.write("*");
+		}
+		document.write("<br>");
+	}
